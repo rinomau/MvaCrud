8661be1f14ea365adf95faeb9477d81fc78c764d
Mauro Rainis <mauro.rainis@gmail.com>
2013-05-25 08:02:33 +0200

    Add base crud service

---
+++ src/Crud/Service/BaseCrudService.php
@@ -0,0 +1,59 @@
+<?php
+
+use Crud\Service\CrudServiceInterface;
+
+namespace MvaModuleTemplate\Service;
+
+class BaseCrudService implements CrudServiceInterface {
+    
+    private $I_entityRepository;
+    private $I_entityManager;
+    private $I_entity;
+    
+    public function __construct($I_entityManager,$s_entityRepository,$I_entity) {
+        $this->I_entityRepository  = $I_entityManager->getRepository($s_entityRepository);
+        $this->I_entityManager = $I_entityManager;
+        $this->I_entity = $I_entity;
+    }
+
+    public function getEntity($i_id){
+        $I_entity = $this->I_entityRepository->find($i_id);
+        
+        if ($I_entity === null ){
+            throw new \Exception('Entity not found');    //@todo throw custom exception type
+        }
+        
+        return $I_entity;
+    }
+
+    public function getAllEntities(){
+        return $this->I_entityRepository->findAll();
+    }
+    
+    public function upsertEntityFromArray(array $am_formData){
+
+        $I_entity = NULL;
+        if (isset($am_formData['id']) && 
+            is_numeric($am_formData['id']) && 
+            $am_formData['id'] > 0){
+            $I_entity = $this->getEntity($am_formData['id']);
+        }
+
+        if ( $I_entity === null ) {
+            $I_entity = $this->I_entity;
+        }
+
+        $I_entity->fillWith($am_formData);
+        
+        $this->I_entityManager->persist($I_entity);
+        $this->I_entityManager->flush();
+    
+        return $I_entity;
+    }
+    
+    public function deleteEntity( $I_entity ) {
+        $this->I_entityManager->remove($I_entity);
+        $this->I_entityManager->flush();
+    }    
+    
+}
---
+++ src/Crud/Service/CrudServiceInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MvaCrud\Service;
+
+interface CrudServiceInterface
+{
+    /**
+     * Get single entity
+     *
+     * @return found entity instance or null
+     */
+    public function getEntity($id);
+    
+    /**
+     * Get all entities
+     *
+     * @return found entities instances or null
+     */
+    public function getAllEntities();
+    
+    /**
+     * Inserts or updates Entity
+     * 
+     * @param array $am_formData
+     * @return Ambigous <NULL, \MvaModuleTemplate\Entity\Dog>
+     */
+    public function upsertEntityFromArray(array $am_formData);
+    
+    public function deleteEntity($I_entity);
+
+}
