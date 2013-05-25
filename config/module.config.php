144bd1ec1ee3b2698b6121c864644702cdfe1150
Mauro Rainis <mauro.rainis@gmail.com>
2013-05-25 08:07:11 +0200

    update config module

--- config/module.config.php
+++ config/module.config.php
@@ -10,6 +10,76 @@
 namespace MvaCrud;
 
 return array(
+    'router' => array(
+        'routes' => array(
+            // The following is a route to simplify getting started creating
+            // new controllers and actions without needing to create a new
+            // module. Simply drop new controllers in, and you can access them
+            // using the path /MvaModuleTemplate/:controller/:action
+            'crud' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/crud',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Crud\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    
+                    // Default routes
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                    
+                    // Default CRUD routes
+                    'action' => array(
+                        'type' => 'Zend\Mvc\Router\Http\Segment',
+                        'options' => array(
+                            'route'    => '/:action[/:id]',
+                            'constraints' => array(
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id'         => '[0-9]*',
+                            ),
+                        ),
+                    ),
+                                        
+                ),
+            ),
+        ),
+    ),
+    
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+            ),
+        ),
+    ),
+    
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
     'controller_plugins' => array(
        'invokables' => array(
             'CrudPlugin' => 'Crud\Controller\Plugin\CrudPlugin'
